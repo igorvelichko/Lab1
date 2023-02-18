1f9895283ceb9c49d10b64629ce8e8d75f66d575
igorvelichko <velichcko.228-2016@yandex.ru>
2023-02-18 17:10:40 +1000

    Laba1

--- main.c
+++ main.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h> 
 int main(int argc, char *argv[]) {
-
-	printf("World of Tanks!\n");
+	double s = 2;
+	int x;
+	float D, a, b, c, x1, x2, sqrtD;
+	printf("Enter a: ");
+	scanf("%f", &a);
+	printf("Enter b: ");
+	scanf("%f", &b);
+	printf("Enter c: ");
+	scanf("%f", &c);
+	printf("%.2fx ^ %.2lf + %.2fx + %.2f = 0 \n", a, s, b, c);
+	D = pow(b, s)-4*a*c;
+	printf("D = %.0f^%.0f - 4 * %.0f * %.0f \n", b, s, a, c);
+	sqrtD = sqrtl(D);
 	
-	int a, b;
-	
-	a = 54;
-	b = 10;
-	printf("%d", a + b);
-	
+	if(D > 0){
+		printf("sqrtD = %.0f \n", sqrtD);
+		x1 = (-b - sqrtD)/(2*a);
+	    x2 = (-b + sqrtD)/(2*a);
+	    printf("x1 - %.2f, x2 - %.2f \n", x1, x2);	
+	}
+	if(D < 0){
+		printf("sqrtD = %.0f \n", sqrtD);
+		printf("Korney net \n");
+	}
+	if(D == 0){
+		printf("sqrtD = %.0f \n", sqrtD);
+	    x1 = -b/(2*a);
+	    printf("x1 - %.2f \n", x1);
+	}
 	return 0;
 	
 	
